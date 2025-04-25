490d3984ca080e62162081e6b7d1abd84fe7d3ab
Lennon Zanchet <lennonzanchet7@gmail.com>
2025-04-24 22:11:16 -0300

    .

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // ← IMPORTANTE: isso aqui é o que carrega o jest-dom corretamente
-};
